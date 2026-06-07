f4fec0c07e61746469c91c68a433aa27e073116c
Arturo Tanzi <arturotanzi05@gmail.com>
2026-06-07 11:00:11 +0200

    Completato Selfwork Identification and Authentication Failures

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,12 @@ trait PasswordValidationRules
      */
     protected function passwordRules(): array
     {
-        return ['required', 'string', Password::default(), 'confirmed'];
+        return ['required', 'string', Password::min(12)
+            ->letters() // Richiede almeno una lettera
+            ->mixedCase() // Richiede sia lettere maiuscole che minuscole
+            ->numbers() // Richiede almeno un numero
+            ->symbols() // Richiede almeno un simbolo
+            ->uncompromised() // Verifica che la password non sia stata compromessa in una violazione dei dati
+        , 'confirmed'];
     }
 }
